828d013bba2785dad1a29b9949076e46b955dd1c
TheOnemoxh <samuelda.molina@gmail.com>
2025-06-17 00:23:20 -0500

    Ultima version.12

--- carpooling_backend/settings.py
+++ carpooling_backend/settings.py
@@ -2,20 +2,19 @@ from datetime import timedelta
 from pathlib import Path
 import dj_database_url
 import os
-
-
+from decouple import config
 
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'insecure-dev-secret-key')
+SECRET_KEY = config('SECRET_KEY')
 
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
+
 APPEND_SLASH = False
-
 
 
 # Aplicaciones instaladas
